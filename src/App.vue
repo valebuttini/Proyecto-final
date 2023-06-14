ae1eeb4e2ab8f4e1c25ac3f0225fc1dab29e09cb
Valentin Buttini <buttiniprocer@hotmail.com>
2023-06-13 21:12:22 -0300

    termina

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app-component">
+    <MiCompo/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MiCompo from './components/MiCompo.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    MiCompo,
+  },
+
+};
 </script>
 
 <style>
-#app {
+#app-component {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/MiCompo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="hello">
+        <h1>Bienvenidos a osteria b-vin</h1>
+        <li>Carrito</li>
+        <li>Catalogo</li>
+        <li>Nosotros</li>
+        <button @click="nuevoPlato">Añadir nuevo plato</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MiCompo',
+
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+
+
+  </style>
